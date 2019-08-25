3dd938fd834225beef80f5168a7012a0044516ee
Jay Piang <43165266+jayarrgp@users.noreply.github.com>
2019-08-25 09:50:24 +0800

    Update fibonacci.py
    add int :D

--- fibonacci.py
+++ fibonacci.py
@@ -5,7 +5,7 @@ fibonacciNumber = []
 firstAddend.append(0)
 secondAddend.append(1)
 fibonacciNumber.append(firstAddend [0]+ secondAddend[0])
-x = input("Please input how many Fibonacci numbers: ")
+x = int(input("Please input how many Fibonacci numbers: "))
 
 for x in range(x-3):
 	fibonacciNumber.append(fibonacciNumber[x] + secondAddend [x])
